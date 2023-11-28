d1402a784100aef2d016e8613d0454351b63ae8c
RamizK01 <ramizk2001@gmail.com>
2023-11-28 14:48:05 -0500

    fixed error

--- EDA.R
+++ EDA.R
@@ -48,9 +48,9 @@ df <- df[,-26]
 # Pre_PTT: 1 missing, we can impute i guess using domain knowledge
 
 ### Creating 24hr blood product variables ###
-df <- df %>% mutate('Total 24hr Plt' = Intra_Platelets + Plt.0.24hrs) %>%
-             mutate('Total 24hr FFP' = Intra_Fresh.Frozen.Plasma + FFP.0.24hrs) %>%
-             mutate('Total 24hr Cryo' = Intra_Cryoprecipitate + Cryo.0.24hrs)
+df <- df %>% mutate('Total 24hr Plt' = Intra_Platelets + `Plt 0-24hrs`) %>%
+             mutate('Total 24hr FFP' = `Intra_Fresh Frozen Plasma` + `FFP 0-24hrs`) %>%
+             mutate('Total 24hr Cryo' = `Intra_Cryoprecipitate` + `Cryo 0-24hrs`)
 
 
 ### EDA REPORT EXPORTED ###
