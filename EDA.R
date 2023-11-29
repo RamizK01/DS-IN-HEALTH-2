3657cd53ffcd1ba71e4004f03a942342c62441cd
RamizK01 <ramizk2001@gmail.com>
2023-11-29 15:56:58 -0500

    RANDOM FOREST WOOOOOOOOOOOO

--- EDA.R
+++ EDA.R
@@ -42,44 +42,12 @@ colSums(is.na(df))
 df <- df[,-26]
 # Death Date and Survival Time: 83.33% missing, no worries this is systematic
 # LAS score is 6.25% missing, imputation?
-library(mice)
-df_PMM <- rename(df, "LAS_score" = "LAS score")
+imp_df <- df[,c(3:14,19)]
+library(missForest)
+imp_df <- as.data.frame(imp_df)
+imp_df <- missForest(imp_df)$ximp
 
-df_PMM <- df[, c(3:25)]
-
-df_PMM <- rename(df_PMM, "alpha1_Antitrypsin_Deficiency" = "alpha1-Antitrypsin Deficiency")
-df_PMM <- rename(df_PMM, "Cystic_Fibrosis" = "Cystic Fibrosis")
-df_PMM <- rename(df_PMM, "Idiopathic_Pulmonary_Hypertension" = "Idiopathic Pulmonary Hypertension")
-df_PMM <- rename(df_PMM, "Interstitial_Lung_Disease" = "Interstitial Lung Disease")
-df_PMM <- rename(df_PMM, "First_Lung_Transplant" = "First Lung Transplant")
-df_PMM <- rename(df_PMM, "Redo_Lung_Transplant" = "Redo Lung Transplant")
-df_PMM <- rename(df_PMM, "ExVIVO_Lung_Perfusion" = "ExVIVO Lung Perfusion")
-df_PMM <- rename(df_PMM, "Preoperative_ECLS" = "Preoperative ECLS")
-df_PMM <- rename(df_PMM, "LAS_score" = "LAS score")
-
-
-# Setting up the method for PMM specifically for LAS_score
-method <- make.method(df_PMM)
-method["LAS_score"] <- "pmm"
-
-# Performing the imputation
-imp <- mice(df_PMM, method = method, m = 20, seed = 123, print = FALSE)
-
-fit <- with(imp, lm(LAS_score ~ Type + Gender + Height + Weight + Age + BMI + COPD + alpha1_Antitrypsin_Deficiency
-                    + Cystic_Fibrosis + Idiopathic_Pulmonary_Hypertension + Interstitial_Lung_Disease + Pulm_Other 
-                    + First_Lung_Transplant + Redo_Lung_Transplant + ExVIVO_Lung_Perfusion + Preoperative_ECLS + Pre_Hb
-                    + Pre_Hct + Pre_Platelets + Pre_PT + Pre_INR + Pre_PTT))
-summary(imp)
-summary(pool(fit))
-df_complete <- complete(imp, 1)
-
-new_LAS_scores <- df_complete[,17]
-
-# Insert new LAS scores
-df$`LAS score` <- new_LAS_scores
-# Duration of ICU stays: 1 missing, mean impute
-mean_icu <- mean(df$`Duration of ICU Stay (days)`, na.rm = TRUE)
-df$`Duration of ICU Stay (days)` <- ifelse(is.na(df$`Duration of ICU Stay (days)`), mean_icu, df$`Duration of ICU Stay (days)`)
+df1 <- df
 
 # Pre_PTT: 1 missing, mean impute
 mean_pre_ptt <- mean(df$Pre_PTT, na.rm = TRUE)
@@ -90,13 +58,11 @@ df <- df %>% mutate('Total 24hr Plt' = Intra_Platelets + `Plt 0-24hrs`) %>%
              mutate('Total 24hr FFP' = `Intra_Fresh Frozen Plasma` + `FFP 0-24hrs`) %>%
              mutate('Total 24hr Cryo' = `Intra_Cryoprecipitate` + `Cryo 0-24hrs`)
 
+### EXPORTING FINAL  CLEANED DATA ###
+write.csv(df, file = "cleaned_transfusion_data.csv", row.names = FALSE)
+
 ### EDA REPORT EXPORTED ###
 library(DataExplorer)
 create_report(df)
 data()
-
-### EXPORTING FINAL  CLEANED DATA ###
-write.csv(df, file = "cleaned_transfusion_data.csv", row.names = FALSE)
-
-
 
