10410f44090f01f432d6970d99ad3b9cd59556c7
tahakhanbhai <taha.khanbhai@mail.utoronto.ca>
2023-11-30 10:20:28 -0500

    SVM code - plotting doesnt work

--- EDA.R
+++ EDA.R
@@ -2,7 +2,7 @@
 df <- readxl::read_excel("transfusion data.xlsx")
 
 # index columns of interest
-df <- df[,c(1,3,4:15,25,26,28:41,46,47,49,50,57,60:65,77:92,116,117)]
+df <- df[,c(1,2,3,4:15,25,26,28:41,46,47,49,50,57,60:65,77:92,116,117)]
 
 ### Rename and convert variables to binary ###
 library(dplyr)
@@ -51,7 +51,79 @@ data()
 
 ### EXPORTING FINALCLEANED DATA ###
 # Assuming your cleaned dataframe is named df
-write.csv(df, file = "cleaned_transfusion_data.csv", row.names = FALSE)
+
+
+df <- read_csv("cleaned_transfusion_data.csv")
+create_report(df)
+
+as.factor(df$Gender)
+# Support Vector Machines
+# load packages
+library(ISLR)
+library(e1071)
+library(cluster)
+library(epiR)
+library(pROC)
+
+# Split data into training and test groups
+as.factor(df$Massive.Transfusion)
+
+set.seed(123)
+
+train.index <- train.I <- sample(nrow(df), round(nrow(df)/2))
+
+train <- df[train.index, c(28,29,58)]
+test <- df[-train.index, c(28,29,58)]
+
+svmfit <- svm(Massive.Transfusion ~ ECLS_ECMO + ECLS_CPB, data = train, kernel = "linear", cost = 1, scale=FALSE, decision.values=T, probability = T)
+
+head(cbind(Actual = test$Massive.Transfusion, Predicted = predictions))
+predictions <- predict(svmfit, newdata = test)
+
+confusion_matrix <- table(Actual = test$Massive.Transfusion, Predicted = predictions)
+
+coefficients <- coef(svmfit)
+
+x <- plot(svmfit, df[, 28:29])
 
 
 
+dat <- data.frame(x=x, y= train$Massive.Transfusion) # Create data frame
+
+
+
+plot(svmfit, iris, Petal.Width ~ Petal.Length,
+     slice = list(Sepal.Width = mean(iris$Sepal.Width), Sepal.Length = mean(iris$Sepal.Length)))
+
+
+set.seed(1)
+x <- matrix(rnorm(20*2), ncol=2)
+y <- c(rep(-1,10), rep(1,10))
+x[y==1,] <- x[y==1,] + 1 # Add 1 to separate classes (Class 1 & -1)
+
+plot(x, col=(3-y)) 
+
+dat <- data.frame(x=x, y=as.factor(y)) # Create data frame
+
+# Run the SVM model
+svmfit <- svm(y ~ ., data=dat , kernel = "linear", cost = 2, scale=FALSE, decision.values=T, probability = T)
+
+plot(svmfit, dat)
+
+
+
+
+
+# predictions with decision values and probabilities
+# Predictions on training data
+pr <- predict(svmfit, dat, decision.values = T, probability = T)
+
+# Confusion Matrix to evaluate the model
+t <- table(pr, dat$y)
+t
+
+# Calculate accuracy
+accuracy <- sum(diag(t))/sum(t)
+accuracy
+
+colSums(is.na(df))
